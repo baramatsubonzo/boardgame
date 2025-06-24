fde160306b4f9ff629b55808815780ffd1ae8d34
Shota Matsubara <baramatsubonzo@gmail.com>
2025-06-24 16:29:49 +0900

    create cell class

---
+++ cell.rb
@@ -0,0 +1,28 @@
+class Cell
+  attr_reader :row, :col, :state
+
+  def initialize(row, col, state = nil)
+    @row = row
+    @col = col
+    @state = state
+  end
+
+  def empty?
+    @state.nil?
+  end
+
+	def place(mark) # 'x'or'o'
+    return false unless empty?
+    @state = mark
+    true
+  end
+
+	def clear
+		@state = nil
+	end
+
+	# 文字に変換する
+	def to_s
+		@state || ''
+	end
+end
